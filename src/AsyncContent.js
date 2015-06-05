b2086abe05807108b6228abc168ccc9d6a72e958
nozhdelov <ivan_nozhdelov@yahoo.com>
2015-06-05 15:55:46 +0300

    sdfds

--- src/AsyncContent.js
+++ src/AsyncContent.js
@@ -6,7 +6,7 @@ ViewComponent.AsyncContent = ViewComponent.register('AsyncContent', {
 		var deferred = Q.defer();
 		
 		setTimeout(function(){
-			deferred.resolve('<h1>wats aaaaaaaaap</h1>');
+			deferred.resolve('<h1>wats aaaaaaaaap</h1> <KWButton></<KWButton>');
 		}, 4000);
 		
 		return deferred.promise;
---
+++ src/DeepNesting.js
@@ -0,0 +1,12 @@
+'use strict';
+
+'use strict';
+
+ViewComponent.DeepNesting = ViewComponent.register('DeepNesting', {
+	
+	render : function() {
+		return '<p > dsfsdfsdf <AsyncContent></AsyncContent></p>';
+	}
+	
+	
+});
