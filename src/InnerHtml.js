f0ec70cc335b9170477ceda3a9f9aa4f59c33c65
nozhdelov <ivan_nozhdelov@yahoo.com>
2015-06-16 08:42:46 +0300

    fdsf

--- src/InnerHtml.js
+++ src/InnerHtml.js
@@ -1,6 +1,21 @@
 'use strict';
 
 ViewComponent.InnerHtml = ViewComponent.register('InnerHtml', {
+	
+	init : function(){
+		var self = this;
+		this.on('render', function(){
+			someSelector().on('click', function(){
+				loadSomeStuff().then(function(res){
+					self.nodeContent = res;
+					self.rerender();
+					someEffect();
+				});
+			});
+		});
+		
+	},
+	
 	render : function(){
 		setTimeout(function(){
 			this.nodeContent = new Date().toString() + '<br>';
